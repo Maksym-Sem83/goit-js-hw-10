5fcee2f97b95a1bb9e5215a5f46b666d5ece6c6f
max_work <max_semenov@ukr.net>
2025-06-05 17:16:05 +0300

    add import, element

---
+++ src/js/1-timer.js
@@ -0,0 +1,13 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const startBtn = document.querySelector("[data-start]");
+const dateValue = document.querySelector("#datetime-picker");
+const daysValue = document.querySelector("[data-days]");
+const hoursValue = document.querySelector("[data-hours]");
+const minutesValue = document.querySelector("[data-minutes]");
+const secondsValue = document.querySelector("[data-seconds]");
+
+
